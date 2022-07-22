4649ea5a748ad6c08c6e0a3fb345d3e19bb2accc
Faheem Hossain <fmhossain2941@gmail.com>
2022-07-23 00:14:47 +0600

    Update moduleWithPrivateFunc.py

--- sample/moduleWithPrivateFunc.py
+++ sample/moduleWithPrivateFunc.py
@@ -1,10 +1,16 @@
+# this file contains two functions- one private and the other non-private
+# the private.py file contains the src/private.py
+# the runme.py is the file which one should run
+
 
 from private import PrivateFunc
 privatefunc = PrivateFunc("moduleWithPrivateFunc")
 
+
 @privatefunc.private
 def prvt_func():
     print("working...")
-    
+  
+
 def non_prvt_func():
     prvt_func()
