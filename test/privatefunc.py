f36eb1571fcd5988d600b92e45a1efa04eeb68a3
Faheem Hossain <fmhossain2941@gmail.com>
2023-01-11 01:05:42 +0600

    Update privatefunc.py

--- test/privatefunc.py
+++ test/privatefunc.py
@@ -1,24 +1,37 @@
 # this file is imported from src/private.py
 
 
+"""
+  Copyright (C) 2021-2023 Md. Faheem Hossain fmhossain2941@gmail.com
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+"""
+
+
 
 class PrivateFunc:
 
-    """
-     >> description: this is a class which one can use to create private functions
-     >> public functions: private
-     >> an example code is given in https://github.com/Faheem41/Private-Function-in-Python/tree/main/test
-     >> source code: https://github.com/Faheem41/Private-Function-in-Python
-    """
-
     __slots__ = ["_filename", "__error_name", "__error_message"]
-    __version__ = '1.4.22'
-    
+    __version__ = '1.5.23'
+
     _filename: str
     __error_name: type
     __error_message: str
 
-      
+
     def __init__(
             self,
             filename,  # name of the file, where the object of PrivateFunc class is created
@@ -36,7 +49,7 @@ class PrivateFunc:
             self.__error_name = ImportError
         self.__error_message = error_message
 
-        
+
     def private(self, func):
 
         """this is the core function of the class"""
@@ -47,7 +60,11 @@ class PrivateFunc:
             from inspect import stack
             import os.path as path
 
-            if self._filename != argv[0]:
+            if self._filename != path.splitext(
+                    path.basename(
+                        argv[0]
+                    )
+            )[0]:
                 # if self._filename == sys.argv[0] that means the function wasn't imported, so it should work
                 # otherwise the  function is imported and we have to check how it is imported,
                 # I mean who is calling it, is it called by a function of its own filename or else
